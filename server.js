45b6737b30b22d1394bbe9ee8aba6d40370f6c77
nlong1 <nathanlong@gmail.com>
2018-08-14 13:57:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var mongoose = require('mongoose');                     // mongoose for mongodb
 var morgan = require('morgan');             // log requests to the console (express4)
 var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+var secret_key = 'some_secret_key'
 
 mongoose.connect('mongodb://localhost/env_mon');
 
